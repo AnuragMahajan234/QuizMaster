acd98e987da14f539d47cd04fc41ed8f70d70ee3
AnuragMahajan234 <anuragmahajan1711@gmail.com>
2019-06-11 14:47:55 +0530

    Update QuizMasterApplication.java

--- src/main/java/com/quiz/mst/QuizMasterApplication.java
+++ src/main/java/com/quiz/mst/QuizMasterApplication.java
@@ -2,10 +2,17 @@ package com.quiz.mst;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class QuizMasterApplication {
+public class QuizMasterApplication extends SpringBootServletInitializer{
 
+		 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	         return application.sources(SpringdemoappApplication.class);
+	    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(QuizMasterApplication.class, args);
 	}
